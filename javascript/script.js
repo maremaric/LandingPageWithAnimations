ca3316a09ac45052c88feab46308e64e39d0e2a2
maremaric <markoreus888@gmail.com>
2020-11-26 18:20:23 +0100

    vezba

--- javascript/script.js
+++ javascript/script.js
@@ -43,3 +43,17 @@ sr.reveal('.animate-bottom', {
     distance: '25rem',
     delay: 600
 });
+
+
+/* header fixed */
+window.onscroll = function() {
+    const docScrollTop = document.documentElement.scrollTop;
+
+    if(window.innerWidth > 991) {
+        if(docScrollTop > 100) {
+            document.querySelector("header").classList.add("fixed");
+        } else {
+            document.querySelector("header").classList.remove("fixed");
+        };
+    };
+};
